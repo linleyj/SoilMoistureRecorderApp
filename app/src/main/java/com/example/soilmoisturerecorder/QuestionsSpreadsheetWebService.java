07b5394f57188455b135d32bc1c6fdbd3cdfcadb
linley <linley.jesson@gmail.com>
2017-01-03 09:46:30 +1300

    Initial commit

---
+++ app/src/main/java/com/example/soilmoisturerecorder/QuestionsSpreadsheetWebService.java
@@ -0,0 +1,13 @@
+package com.example.soilmoisturerecorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionsSpreadsheetWebService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_spreadsheet_web_service);
+    }
+}
